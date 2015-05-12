adfd019b5880cf0f1aa28a05968f040b17b1e7bd
Siim Liiser <siim@wisemedia.ee>
2015-05-12 17:17:47 +0300

    new tab and youtube image change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def setting_link(icon, setting)
     s = Setting.value(setting)
-    link_to(fa_icon(icon), s) if s
+    link_to(fa_icon(icon), s, target: '_blank') if s
   end
 end
